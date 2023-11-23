85719fcf0db9d268d37f2e22a9ec92cb971e2138
Li889654qq <150346917+Li889654qq@users.noreply.github.com>
2023-11-23 11:56:19 +0000

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,24 @@
+
+
+function setup() {
+    var canvas = createCanvas (800, 800);
+    canvas.parent("p5container");
+    background(200);
+    for(i=0; i<25; i++){
+     var circle0bj = {
+        x: random(width),
+        y: random(height),
+        r: 30
+    } circleArr.push(circle0bj)
+        
+   for(var j =0; j<circleArrat. length; j++){
+
+        ellipse(circleArr[j].x, circleArr[j].y, circleArr[j].r)
+    }
+    }
+
+
+}
+function draw() {
+    
+}
